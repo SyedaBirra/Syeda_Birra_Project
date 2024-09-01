2f30e14e6ae4a8d73d236e108449f9ac7dd2c60d
SyedaBirra <158575451+SyedaBirra@users.noreply.github.com>
2024-09-01 19:03:06 +0200

    working on innit function

--- Bloom_filter.py
+++ Bloom_filter.py
@@ -6,7 +6,12 @@ import random
 class BloomFilter:
     def __init__(self, n, hash_count):
         # n is the number of items , hash_count is the no. of hash functions
-        pass
+        self.size=int((2*n)/math.log(2))  
+        self.hash_count=hash_count
+        self.bit_array=bitarray.bitarray(self.size)
+        self.bit_array.setall(0)
+        self.seeds=[random.randint(1,100)for _ in range(hash_count) ]  
+
     
     def hashes(self,item):
         #Generating multiple hash values

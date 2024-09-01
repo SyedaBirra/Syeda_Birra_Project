529960925d7412b7195f34ef829af43327959bed
SyedaBirra <158575451+SyedaBirra@users.noreply.github.com>
2024-09-01 18:37:40 +0200

    comments

--- Bloom_filter.py
+++ Bloom_filter.py
@@ -4,10 +4,14 @@ import bitarray
 
 class BloomFilter:
     def __init__(self, n, hash_count):
+        # n is the number of items , hash_count is the no. of hash functions
         pass
     
+    #To add an item to BloomFilter
     def add(self, item):
+        
         pass
     
+    #To check an item in the Bloom filter
     def check(self, item):
         pass

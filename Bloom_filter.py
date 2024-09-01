5015eda6ae73b427738f6e1bdaa70e0f3e65f901
SyedaBirra <158575451+SyedaBirra@users.noreply.github.com>
2024-09-01 18:32:55 +0200

    class BloomFilter

--- Bloom_filter.py
+++ Bloom_filter.py
@@ -2,3 +2,12 @@ import mmh3
 import math
 import bitarray
 
+class BloomFilter:
+    def __init__(self, n, hash_count):
+        pass
+    
+    def add(self, item):
+        pass
+    
+    def check(self, item):
+        pass
